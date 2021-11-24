d89442e7a7474479713ffc2134544dbdba49a5e2
josef <josef.aidt@gmail.com>
2021-11-24 13:17:12 -0600

    🚀

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifystorageoverri22178bca": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s33b4bf5ec": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
